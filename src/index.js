bed19b1fea5c15f3b5c841d1a22d9d94f9d1079d
ICURAIC-LL3ANTO\Administrator <rouge16888@gmail.com>
2018-03-21 19:49:58 +0800

    0.0.2完整骨架结构

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducer'
+// 全局reducer
+
+import './config'
+// 路由拦截器
+
+// compose 是为了加入移步中间件 以及window的调试;
+const store = createStore(reducers, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension?window.devToolsExtension():f=>f
+))
+
+ReactDOM.render(
+  (<Provider store={store}>
+      <BrowserRouter>
+        <h1>123</h1>
+      </BrowserRouter>
+    {/* <App /> */}
+</Provider>), document.getElementById('root'));
 registerServiceWorker();
